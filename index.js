183f18b4a04be6528da0045b118dd11c594135fe
anaghrajdev007 <unknowndev007@gmail.com>
2025-03-29 12:51:48 +0530

    Some

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetchWebStories = require('./scrape');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3060;
 
 app.get('/api/web-stories', async (req, res) => {
   const stories = await fetchWebStories();
